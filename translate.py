a7693d1a6b829f2ae5483c7f07c8da08c7533e05
Andrey N. Krasnov <andrey.n.krasnov@yadro.com>
2024-01-01 18:27:49 +0300

    fix translations

--- translate.py
+++ translate.py
@@ -1,42 +1,21 @@
-import locale
+from locale import getdefaultlocale
+from messages import messages
+from logger import create_logger
 
-current_locale = locale.getdefaultlocale()
-lang = current_locale[0] or "en_US"
+logger = create_logger("translate")
 
-messages = {
-    "en_US": {
-        "bot": {
-            "your_tutor": "Your Assistant: `{tutor}`",
-            "not_allowed": "*NOT ALLOWED*, id=`{id}`",
-            "welcome": "Hi, *{name}*!\n```{id}```",
-            "new_chat": "`Context cleared`",
-            "new_tutor": "Select new Assistant:\n{tutors}",
-            "error_in_the_code": "`error 42`"
-        },
-        "gpt": {
-            "instructions": "Please address the user as {name}. The user has a premium account."
-        }
-    },
-    "ru_RU": {
-        "bot": {
-            "your_tutor": "Ваш Ассистент: `{tutor}`",
-            "not_allowed": "*НЕТ ДОСТУПА*, id=`{id}`",
-            "welcome": "Привет, *{name}*!\n```{id}```",
-            "new_chat": "`История очищена`",
-            "new_tutor": "Выберите нового Ассистента:\n{tutors}",
-            "error_in_the_code": "`ошибка 42`"
-        },
-        "gpt": {
-            "instructions": "Пожалуйста, обращайтесь к пользователю как {name}. У пользователя премиум-аккаунт."
-        }
-    }
-}
+lang = getdefaultlocale()[0]
+lang = lang if lang in messages else "en_US"
+translations = messages[lang]
 
 
 def _t(path, **kwargs):
   keys = path.split('.')
-  translation = messages[lang]
+  translation = translations
   for key in keys:
+    if key not in translation:
+      logger.error(f"missing translation: {path}")
+      return f"`{path}`"
     translation = translation[key]
 
   return translation.format(**kwargs)
