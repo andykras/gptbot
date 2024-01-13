31246f4bda6e9a9cdcc6217b750933926d6676a8
Andrey N. Krasnov <andrey.n.krasnov@yadro.com>
2024-01-13 04:52:51 +0300

    update consts

--- env.py
+++ env.py
@@ -8,7 +8,6 @@ API_KEY = getenv("API_KEY")
 
 # settings
 # GROUP_ID = -123123 # set this if you want bot to work in chats\groups
-THREADS_RUN_WAIT_SLEEP = 1  # in seconds
 RUN_STATUS_POLL_INTERVAL = 1
 GROUP_BOT_RESPONSE_DELAY = 20
 BOT_RESPONSE_DELAY = 5
