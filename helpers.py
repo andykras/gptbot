f1fbb53eed3110281c3e2907cb43b5435c6a36e7
Andrey Krasnov <root@andykras.org>
2024-01-14 15:42:38 +0300

    send 'typing' when thread runs (#31)

--- helpers.py
+++ helpers.py
@@ -1,3 +1,8 @@
+import time
+from enum import Enum
+from aiogram import types
+
+
 def get_unclosed_tag(markdown):
   tags = ["```", "`", "*", "_"]
   current_tag = ""
@@ -36,3 +41,32 @@ def escape_markdown(text):
       text = text.replace(char, "\\" + char)
 
   return text
+
+
+class ChatActions:
+  last_sent_time = {}
+
+  class Action(Enum):
+    TYPING = "typing"
+    UPLOAD_PHOTO = "upload_photo"
+    RECORD_VIDEO = "record_video"
+    UPLOAD_VIDEO = "upload_video"
+    RECORD_VOICE = "record_voice"
+    UPLOAD_VOICE = "upload_voice"
+    UPLOAD_DOCUMENT = "upload_document"
+    FIND_LOCATION = "find_location"
+    RECORD_VIDEO_NOTE = "record_video_note"
+    UPLOAD_VIDEO_NOTE = "upload_video_note"
+
+  @classmethod
+  async def send(cls, message: types.Message, action: Action = Action.TYPING):
+    current_time = time.time()
+    chat_id = message.chat.id
+
+    if chat_id not in cls.last_sent_time or current_time - cls.last_sent_time[chat_id] > 5:
+      await message.bot.send_chat_action(chat_id, action)
+      cls.last_sent_time[chat_id] = current_time
+
+  @classmethod
+  async def send_typing(cls, message: types.Message):
+    await cls.send(message, cls.Action.TYPING)
